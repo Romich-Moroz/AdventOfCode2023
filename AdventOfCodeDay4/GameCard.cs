182a4c758d0cc4215b32b296f8b26e8513025440
Roman <romich651@gmail.com>
2023-12-05 07:52:32 +0300

    Code cleanup

---
+++ AdventOfCodeDay4/GameCard.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCodeDay4
+{
+    public class GameCard(int gameCardId, List<int> winningScratchCards, List<int> playerScratchCards)
+    {
+        public int GameCardId { get; set; } = gameCardId;
+        public List<int> WinningScratchCards { get; set; } = winningScratchCards;
+        public List<int> PlayerScratchCards { get; set; } = playerScratchCards;
+        public int TimesToProcess { get; set; } = 1;
+
+        public static GameCard Parse(string input)
+        {
+            var gameCardSplit = new Regex("[ ]{2,}", RegexOptions.None).Replace(input, " ").Split(':', StringSplitOptions.TrimEntries);
+            var scratchCardSidesSplit = gameCardSplit[1].Split('|', StringSplitOptions.TrimEntries);
+
+            return new(
+                int.Parse(gameCardSplit[0].Split(' ')[1]),
+                scratchCardSidesSplit[0].Split(' ').Select(int.Parse).ToList(),
+                scratchCardSidesSplit[1].Split(' ').Select(int.Parse).ToList()
+           );
+        }
+
+        public int GetWinningCardsCount() => PlayerScratchCards.Where(WinningScratchCards.Contains).Count();
+    }
+}
--- AdventOfCodeDay4/Program.cs
+++ AdventOfCodeDay4/Program.cs
@@ -1,32 +1,7 @@
-﻿using System.Text.RegularExpressions;
-
-namespace AdventOfCodeDay4
+﻿namespace AdventOfCodeDay4
 {
     internal class Program
     {
-
-        public class GameCard(int gameCardId, List<int> winningScratchCards, List<int> playerScratchCards)
-        {
-            public int GameCardId { get; set; } = gameCardId;
-            public List<int> WinningScratchCards { get; set; } = winningScratchCards;
-            public List<int> PlayerScratchCards { get; set; } = playerScratchCards;
-            public int TimesToProcess { get; set; } = 1;
-
-            public static GameCard Parse(string input)
-            {
-                var gameCardSplit = new Regex("[ ]{2,}", RegexOptions.None).Replace(input, " ").Split(':', StringSplitOptions.TrimEntries);
-                var scratchCardSidesSplit = gameCardSplit[1].Split('|', StringSplitOptions.TrimEntries);
-
-                return new(
-                    int.Parse(gameCardSplit[0].Split(' ')[1]),
-                    scratchCardSidesSplit[0].Split(' ').Select(int.Parse).ToList(),
-                    scratchCardSidesSplit[1].Split(' ').Select(int.Parse).ToList()
-               );
-            }
-
-            public int GetWinningCardsCount() => PlayerScratchCards.Where(WinningScratchCards.Contains).Count();
-        }
-
         private static void Main(string[] args)
         {
             var lines = File.ReadAllLines("../../../input.txt");
