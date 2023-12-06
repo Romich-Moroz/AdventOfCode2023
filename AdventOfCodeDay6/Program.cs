8eecbeeadd832ac2ce8d54dbd93158ddb242393b
Roman <romich651@gmail.com>
2023-12-06 08:29:42 +0300

    Add day 6 tasks

---
+++ AdventOfCodeDay6/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace AdventOfCodeDay6
+{
+    public class RaceInfo(long duration, long distanceRecord)
+    {
+        public long GetTotalWaysToWin() => Enumerable.Range(0, (int)duration).Where(i => ((duration - i) * (1 * i)) > distanceRecord).Count();
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            List<RaceInfo> task1 = [new(62, 644), new(73, 1023), new(75, 1240), new(65, 1023)];
+            List<RaceInfo> task2 = [new(62737565, 644102312401023)];
+
+            Console.WriteLine(task1.Select(r => r.GetTotalWaysToWin()).Aggregate((x, y) => x * y));
+            Console.WriteLine(task2.Select(r => r.GetTotalWaysToWin()).Aggregate((x, y) => x * y));
+        }
+    }
+}
